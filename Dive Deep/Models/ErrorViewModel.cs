65008f2efc518158bfa67a518ccd375022c5501e
cuipin <chrispinabimana@gmail.com>
2026-09-01 09:40:34 +0200

    Add project files.

---
+++ Dive Deep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dive_Deep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
