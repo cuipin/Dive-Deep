ed92f2a5536128051c37e82ac944514b45f8e605
cuipin <chrispinabimana@gmail.com>
2026-09-14 17:07:07 +0200

    #
    Add Login page with controller, view, and navigation link
    
    Replaced the "Cart" link in `_Layout.cshtml` with a "Login" link, including a profile icon (`Profile.png`). Added `LoginController` with an `Index` action to handle requests for the "Login" page. Created a new `Index.cshtml` view with a styled login form, including fields for "Username" and "Password," and a submit button.

---
+++ Dive Deep/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dive_Deep.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
